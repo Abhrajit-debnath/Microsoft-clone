91a12cf8574bdaf799330a284f4a7b53abb2ae2a
Abhrajit <abhrajit>
2024-04-08 17:38:11 +0530

    initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // Add other PostCSS plugins here if needed
+  ],
+};
